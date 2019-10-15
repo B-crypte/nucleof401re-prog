798fb83186ae8109bfd5162cd62bd08132f1d1b0
M2507_Yuki <fujiura2507@outlook.jp>
2019-10-15 19:18:58 +0900

    回転方向判別変数（dir）を追加 dirを外部ファイルでも参照できる関数の作成

--- encoder_magnet/encoder_control.cpp
+++ encoder_magnet/encoder_control.cpp
@@ -8,13 +8,14 @@ InterruptIn Enb(D8,PullUp);   //quadrature B phase
 //エンコーダカウンタ
 volatile static int enc_count;   //現在の値
 volatile static int old_count;   //enc_count更新前の値を保持
+static int dir;
 
 //カウンタ初期化
-int init_enc(void)
-{
+int init_enc(void){
     enc_count = 0;
     old_count = 0;
-    
+    dir = 0;
+
     Ena.rise(&encoder_a_cnter);   //A相立ち上がり
     Enb.rise(&encoder_b_cnter);   //B相立ち上がり
     Ena.fall(&encoder_a_cnter);   //A相立ち下がり
@@ -35,8 +36,10 @@ void encoder_a_cnter(void){
 
     if(rot == 1){   //正転
         enc_count++;      //カウントInc
+        dir = 0;
     }else{          //逆転
         enc_count--;      //カウントDec
+        dir = 1;
     }
     if(enc_count == ENCODER_CPR) { //カウント範囲の補正
         //enc_count = 0;
@@ -48,34 +51,36 @@ void encoder_b_cnter(void){
 
     if(rot == 1){   //逆転
         enc_count--;      //カウントDec
+        dir = 1;
     }else{          //正転
         enc_count++;      //カウントInc
+        dir = 0;
     }
     if(enc_count == ENCODER_CPR) { //カウント範囲の補正
         //enc_count = 0;
     }
 }
 //カウント値取得
-int getcnt_enc(int *pi_now,int *pi_old)
-{
+int getcnt_enc(int *pi_now,int *pi_old){
     *pi_now = enc_count;
     *pi_old = old_count;
     return 0;
 }
 //カウント記録
-int rem_enc(void)
-{
+int rem_enc(void){
     old_count = enc_count;
     return 0;
 }
 //エンコーダカウンタ任意値設定
-int setcnt_enc(int set)
-{
+int setcnt_enc(int set){
     enc_count = set;
     return 0;
 }
 //カウント値読み込み
-int readcnt_enc(void)
-{
+int readcnt_enc(void){
     return enc_count;
+}
+//
+int encoder_dir(void){
+    return dir;
 }
